a73c68a3f5e3a11a59bf390d515d22b4e4776c3c
pavzel <Pavel.V.Zelenin@gmail.com>
2020-10-26 17:25:09 +0000

    Added generation of a one-operation arythmetic problem

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,59 @@
+
+
+function increaseFunc(a, b) {
+    return [a + 1, b + 1];
+}
+
+function generateRandomZ(minN, maxN) {
+    return minN + Math.floor(Math.random() * (maxN - minN + 1));
+}
+
+function chooseOperation() {
+    let operations = ['+', '-', '*', '/'];
+    return operations[generateRandomZ(0, 3)];
+}
+
+function generateProblemOneOperation(maxValue1, maxValue2, operation) {
+    let a = generateRandomZ(0, maxValue1);
+    let b = generateRandomZ(0, maxValue2);
+    if (operation == '/') {
+        a = a * b;
+    }
+    output = `${a} ${operation} ${b}`;
+    return output;
+}
+
+/* Initialise the game. */
+function initializeGame() {
+    $("#content").hide();
+
+}
+
 /* The main part of script. */
 $(document).ready(function () {
-    let maxNumber = 100;
-    let a = Math.floor(Math.random() * (maxNumber + 1));
-    let b = Math.floor(Math.random() * (maxNumber + 1));
-    problem = `${a} + ${b}`;
-    solution = eval(problem);
-    $("#content").html(problem + " = " + solution);
+    const maxNumber = 100;
+
+    initializeGame();
+
+    $("#btn-answer").click(function() {
+        $("#message").text("You answered! " + $("#answer").val());
+    });
+
+    $("#new").click(function() {
+        let operation = chooseOperation();
+        problem = generateProblemOneOperation(maxNumber, maxNumber, operation);
+        solution = eval(problem);
+        $("#problem").html(problem + " = ");
+        $("#answer").val("");
+        $("#message").text("Solve a problem:");
+        $("#content").show();
+    });
+
+    $("#stop").click(function() {
+        $("#content").hide();
+        $("#new").hide();
+        $("#stop").hide();
+        $("#message").text("Game is OVER!");
+        console.log("FINISH!");
+    });
 });
